0e413d7a82e9670bff880ea0f7a659fad8e317b1
David Milanese <milanese.david@gmail.com>
2022-01-22 00:43:38 +1030

    chore: remove extranious environment variable

--- server/common/server.ts
+++ server/common/server.ts
@@ -30,7 +30,7 @@ export default class ExpressServer {
       process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
       process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true'
     );
-    app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec));
+    app.use('/spec', express.static(apiSpec));
     app.use(
       OpenApiValidator.middleware({
         apiSpec,
